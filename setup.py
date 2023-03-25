4c6f536b06d607c7c1de05b7c1856adedf2d8194
sumit <sumit@localhost.localdomain>
2023-03-25 12:50:10 -0500

    create or replace table

--- setup.py
+++ setup.py
@@ -1,63 +1,64 @@
-# +--------------------------------------------------------------------------+
-# |  Licensed Materials - Property of IBM                                    |
-# |                                                                          |
-# | (C) Copyright IBM Corporation 2009-2014.                                      |
-# +--------------------------------------------------------------------------+
-# | Licensed under the Apache License, Version 2.0 (the "License");          |
-# | you may not use this file except in compliance with the License.         |
-# | You may obtain a copy of the License at                                  |
-# | http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable |
-# | law or agreed to in writing, software distributed under the License is   |
-# | distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY |
-# | KIND, either express or implied. See the License for the specific        |
-# | language governing permissions and limitations under the License.        |
-# +--------------------------------------------------------------------------+
-# | Authors: Ambrish Bhargava, Tarun Pasrija, Rahul Priyadarshi, Steven James|
-# +--------------------------------------------------------------------------+
-
+import os
 import sys
+from setuptools import setup, find_packages
+#import get_suite
 
-from setuptools import setup, find_packages, find_namespace_packages
-from distutils.core import setup, Extension
-
-PACKAGE = 'django-pyodbc-iseries'
-# VERSION = __import__('django_iseries').__version__
+DESCRIPTION = "Django ISeries DB driver"
 VERSION = '1.0.0'
-LICENSE = 'Apache License 2.0'
-# DESCRIPTION = __import__('django_iseries').__doc__
-DESCRIPTION = ''
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    pass
 
-print(find_packages(where='src'))
+requirements = [
+ 'pyodbc>=4.0.27', 
+ 'django>=2.2.0',
+  
+  'sqlparse',
+
+]
+
+# python setup.py publish
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    sys.exit()
+    
+if sys.argv[-1] == 'egg_info':
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
+
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+# https://packaging.python.org/en/latest/guides/distributing-packages-using-setuptools/
 
 setup(
-    name=PACKAGE,
+    name='django-pyodbc-iseries',
     version=VERSION,
-    license=LICENSE,
-    platforms='All',
-    install_requires=['pyodbc>=4.0.27', 'django>=2.2.0', 'pytz', 'sqlparse'],
-    dependency_links=['https://pypi.org/project/pyodbc/', 'http://pypi.python.org/pypi/Django/',
-                      'https://www.ibm.com/support/pages/ibm-i-access-client-solutions'],
-    description=DESCRIPTION,
-    long_description=DESCRIPTION,
-    author='Ambrish Bhargava, Tarun Pasrija, Rahul Priyadarshi, Steven James',
-    author_email='steven@waitforitjames.com',
-    maintainer='Steven James',
-    maintainer_email='steven@waitforitjames.com',
-    url='https://github.com/soundstripe/django-pyodbc-iseries',
-    keywords='django iseries backends adapter IBM Data Servers database db2',
+
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    classifiers=['Development Status :: 4 - Beta',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: Apache Software License',
-                 'Operating System :: Microsoft :: Windows :: Windows NT/2000',
-                 'Operating System :: Unix',
-                 'Operating System :: POSIX :: Linux',
-                 'Operating System :: MacOS',
-                 'Topic :: Database :: Front-Ends'],
-    data_files=[('', ['./README.md']),
-                ('', ['./CHANGES']),
-                ('', ['./LICENSE'])],
-    zip_safe=False,
+
     include_package_data=True,
+    author='Sumit',
+    author_email='onlysumitg@gmail.com',
+    url='',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/x-rst',
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+    install_requires=requirements,
+    python_requires = '>=3.8',
 )
